925966540b107de2e0973e69319512f5467737f0
Axel Villalobos Garcia <20253ds056@utez.edu.mx>
2026-05-29 14:29:15 -0600

    [UPDATE] Practica completa

---
+++ script.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById("btn");
+var clicked = false;
+btn.addEventListener("click", () => {
+    if (clicked) {
+        return;
+    }
+    const contact = document.getElementById("contact");
+    const mensaje = document.createElement("p");
+    const textfield = document.getElementById("textfield");
+    mensaje.textContent = textfield.value;
+    mensaje.style.color = "#d3d3d3";
+    mensaje.style.fontWeight = "bold";
+    contact.appendChild(mensaje);
+    btn.disabled = true;
+    textfield.disabled = true;
+    btn.style.backgroundColor = "#444c57";
+    btn.textContent = "Mensaje enviado";
+    btn.style.cursor = "default";
+    clicked = true;
+});
